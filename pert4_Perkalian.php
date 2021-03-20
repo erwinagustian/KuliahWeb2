36fc3e2b35a4293745a6eed7b142f8ccbe8308bc
erwinagustian <80106045+erwinagustian@users.noreply.github.com>
2021-03-20 11:06:48 +0700

    Add files via upload

---
+++ pert4_Perkalian.php
@@ -0,0 +1,11 @@
+<?php
+    for ($a = 1; $a <= 1; $a++)
+    {
+        for ($b = 1; $b <= 10; $b++)
+         {
+            $angka = $a * $b;
+            echo $a. "x".$b."=".$angka."</br>";
+         }
+        echo "</br>";
+    }
+?>
