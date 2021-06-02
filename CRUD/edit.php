210712940efb536040fa0745fd0f5e11ad1bcb98
erwinagustian <80106045+erwinagustian@users.noreply.github.com>
2021-06-02 20:44:21 +0700

    Add files via upload

---
+++ CRUD/edit.php
@@ -0,0 +1,119 @@
+<html>
+<head>
+	<title>Add Users</title>
+</head>
+ 
+<body>
+	<a href="index.php">Go to Home</a>
+	<br/><br/>
+ 
+	<form action="add.php" method="post" name="form1">
+		<table width="25%" border="0">
+			<tr> 
+				<td>Name</td>
+				<td><input type="text" name="name"></td>
+			</tr>
+			<tr> 
+				<td>Email</td>
+				<td><input type="text" name="email"></td>
+			</tr>
+			<tr> 
+				<td>Mobile</td>
+				<td><input type="text" name="mobile"></td>
+			</tr>
+			<tr> 
+				<td></td>
+				<td><input type="submit" name="Submit" value="Add"></td>
+			</tr>
+		</table>
+	</form>
+	
+	<?php
+ 
+	// Check If form submitted, insert form data into users table.
+	if(isset($_POST['Submit'])) {
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$mobile = $_POST['mobile'];
+		
+		// include database connection file
+		include_once("config.php");
+				
+		// Insert user data into table
+		$result = mysqli_query($mysqli, "INSERT INTO users(name,email,mobile) VALUES('$name','$email','$mobile')");
+		
+		// Show message when user added
+		echo "User added successfully. <a href='index.php'>View Users</a>";
+	}
+	?>
+</body>
+</html>
+File add.php berfungsi untuk menambahkan pengguna baru. Formulir HTML digunakan untuk menerima masukan data pengguna. Setelah data pengguna diserahkan, MySQL INSERT Query digunakan untuk memasukkan data pengguna ke dalam database.
+
+5. Membuat file edit.php
+<?php
+// include database connection file
+include_once("config.php");
+ 
+// Check if form is submitted for user update, then redirect to homepage after update
+if(isset($_POST['update']))
+{	
+	$id = $_POST['id'];
+	
+	$name=$_POST['name'];
+	$mobile=$_POST['mobile'];
+	$email=$_POST['email'];
+		
+	// update user data
+	$result = mysqli_query($mysqli, "UPDATE users SET name='$name',email='$email',mobile='$mobile' WHERE id=$id");
+	
+	// Redirect to homepage to display updated user in list
+	header("Location: index.php");
+}
+?>
+<?php
+// Display selected user data based on id
+// Getting id from url
+$id = $_GET['id'];
+ 
+// Fetech user data based on id
+$result = mysqli_query($mysqli, "SELECT * FROM users WHERE id=$id");
+ 
+while($user_data = mysqli_fetch_array($result))
+{
+	$name = $user_data['name'];
+	$email = $user_data['email'];
+	$mobile = $user_data['mobile'];
+}
+?>
+<html>
+<head>	
+	<title>Edit User Data</title>
+</head>
+ 
+<body>
+	<a href="index.php">Home</a>
+	<br/><br/>
+	
+	<form name="update_user" method="post" action="edit.php">
+		<table border="0">
+			<tr> 
+				<td>Name</td>
+				<td><input type="text" name="name" value=<?php echo $name;?>></td>
+			</tr>
+			<tr> 
+				<td>Email</td>
+				<td><input type="text" name="email" value=<?php echo $email;?>></td>
+			</tr>
+			<tr> 
+				<td>Mobile</td>
+				<td><input type="text" name="mobile" value=<?php echo $mobile;?>></td>
+			</tr>
+			<tr>
+				<td><input type="hidden" name="id" value=<?php echo $_GET['id'];?>></td>
+				<td><input type="submit" name="update" value="Update"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
